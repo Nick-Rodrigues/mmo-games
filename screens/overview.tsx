f7f7bd900bde1182a72a0619be5a648a5ada6d19
Nick-Rodrigues <RM551060@fiap.com.br>
2024-04-22 21:30:10 -0300

    First commit.

--- screens/overview.tsx
+++ screens/overview.tsx
@@ -1,34 +1,67 @@
+import React, { useState, useEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
-import { ScreenContent } from 'components/ScreenContent';
-import { StyleSheet, View } from 'react-native';
-
-import { Button } from '../components/Button';
+import {Image, Text, FlatList, TouchableOpacity, ActivityIndicator,StyleSheet, View } from 'react-native';
 import { RootStackParamList } from '../navigation';
 
-type OverviewScreenNavigationProps = StackNavigationProp<RootStackParamList, 'Overview'>;
+const Overview = ({ navigation }) => {
+  const [games, setGames] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-export default function Overview() {
-  const navigation = useNavigation<OverviewScreenNavigationProps>();
+  useEffect(() => {
+    const fetchGames = async () => {
+      try {
+        const response = await fetch(
+          'https://www.mmobomb.com/api1/games'
+        );
+        const json = await response.json();
+        setGames(json);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchGames();
+  }, []);
+
+  if (loading) {
+    return <ActivityIndicator size="large" />;
+  }
 
   return (
-    <View style={styles.container}>
-      <ScreenContent path="screens/overview.tsx" title="Overview" />
-      <Button
-        onPress={() =>
-          navigation.navigate('Details', {
-            name: 'Dan',
-          })
-        }
-        title="Show Details"
-      />
-    </View>
+    <FlatList
+    data={games}
+    keyExtractor={(item) => item.id.toString()}
+    renderItem={({item }) => (
+      <TouchableOpacity
+        style={styles.gameContainer}
+        onPress={() => navigation.navigate('Details', {gamesDetails: item})}>
+        {item.thumbnail ? <Image source={{ uri: item.thumbnail }} style={styles.image} /> : null}
+        <Text style={styles.text}>{item.title}</Text>
+      </TouchableOpacity>
+    )}
+    />
   );
-}
+};
 
-export const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 24,
+export default Overview;
+
+const styles = StyleSheet.create({
+  gameContainer: {
+    marginHorizontal: 16,
+    marginVertical: 8,
+    padding: 16,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#ddd',
+    backgroundColor: '#f9f9f9',
+    flexDirection: 'row',
+    alignItems: 'center',
   },
+  image: { width: 80, height: 80, borderRadius: 5 },
+  textContainer: { marginLeft: 12 },
+  title: { fontSize: 18, fontWeight: 'bold' },
+  genre: { fontSize: 14, marginTop: 4, color: '#888' },
 });
